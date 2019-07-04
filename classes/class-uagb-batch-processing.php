c54d2dde2b67ef2ae7a2df1fab391ff37eee3eb6
vrundak <vrundak@bsf.io>
2019-07-04 13:27:15 +0530

    wip

---
+++ classes/class-uagb-batch-processing.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Batch Processing
+ *
+ * @package UAGB
+ * @since x.x.x
+ */
+
+if ( ! class_exists( 'UAGB_Batch_Processing' ) ) :
+
+	/**
+	 * UAGB_Batch_Processing
+	 *
+	 * @since x.x.x
+	 */
+	class UAGB_Batch_Processing {
+
+		/**
+		 * Instance
+		 *
+		 * @since x.x.x
+		 * @var object Class object.
+		 * @access private
+		 */
+		private static $instance;
+
+		/**
+		 * Process All
+		 *
+		 * @since x.x.x
+		 * @var object Class object.
+		 * @access public
+		 */
+		public static $process_all;
+
+		/**
+		 * Initiator
+		 *
+		 * @since x.x.x
+		 * @return object initialized object of class.
+		 */
+		public static function get_instance() {
+			if ( ! isset( self::$instance ) ) {
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * Constructor
+		 *
+		 * @since x.x.x
+		 */
+		public function __construct() {
+
+			// Core Helpers - Image.
+			// @todo 	This file is required for Elementor.
+			// Once we implement our logic for updating elementor data then we'll delete this file.
+			require_once ABSPATH . 'wp-admin/includes/image.php';
+
+			// Core Helpers - Image Downloader.
+			require_once ASTRA_SITES_DIR . 'inc/importers/batch-processing/helpers/class-astra-sites-image-importer.php';
+
+			// Core Helpers - Batch Processing.
+			require_once ASTRA_SITES_DIR . 'inc/importers/batch-processing/helpers/class-wp-async-request.php';
+			require_once ASTRA_SITES_DIR . 'inc/importers/batch-processing/helpers/class-wp-background-process.php';
+			require_once ASTRA_SITES_DIR . 'inc/importers/batch-processing/helpers/class-wp-background-process-astra.php';
+
+			require_once ASTRA_SITES_DIR . 'inc/importers/batch-processing/class-astra-sites-batch-processing-gutenberg.php';
+
+			// Prepare Misc.
+			require_once ASTRA_SITES_DIR . 'inc/importers/batch-processing/class-astra-sites-batch-processing-misc.php';
+
+			self::$process_all = new WP_Background_Process_Astra();
+
+			// Start image importing after site import complete.
+			add_action( 'astra_sites_import_complete', array( $this, 'start_process' ) );
+		}
+
+		/**
+		 * Start Image Import
+		 *
+		 * @since x.x.x
+		 *
+		 * @return void
+		 */
+		public function start_process() {
+
+			error_log( 'Batch Process Started!' );
+
+			// Add "gutenberg" in import [queue].
+			self::$process_all->push_to_queue( UAGB_Batch_Processing_Gutenberg::get_instance() );
+
+			// Add "misc" in import [queue].
+			self::$process_all->push_to_queue( UAGB_Batch_Processing_Misc::get_instance() );
+
+			// Dispatch Queue.
+			self::$process_all->save()->dispatch();
+		}
+
+		/**
+		 * Get all post id's
+		 *
+		 * @since x.x.x
+		 *
+		 * @param  array $post_types Post types.
+		 * @return array
+		 */
+		public static function get_pages( $post_types = array() ) {
+
+			if ( $post_types ) {
+				$args = array(
+					'post_type'      => $post_types,
+
+					// Query performance optimization.
+					'fields'         => 'ids',
+					'no_found_rows'  => true,
+					'post_status'    => 'publish',
+					'posts_per_page' => -1,
+				);
+
+				$query = new WP_Query( $args );
+
+				// Have posts?
+				if ( $query->have_posts() ) :
+
+					return $query->posts;
+
+				endif;
+			}
+
+			return null;
+		}
+
+		/**
+		 * Get Supporting Post Types..
+		 *
+		 * @since x.x.x		 * @param  integer $feature Feature.
+		 * @return array
+		 */
+		public static function get_post_types_supporting( $feature ) {
+			global $_wp_post_type_features;
+
+			$post_types = array_keys(
+				wp_filter_object_list( $_wp_post_type_features, array( $feature => true ) )
+			);
+
+			return $post_types;
+		}
+
+	}
+
+	/**
+	 * Kicking this off by calling 'get_instance()' method
+	 */
+	UAGB_Batch_Processing::get_instance();
+
+endif;
